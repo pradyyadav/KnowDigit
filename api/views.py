e07ca688135d5c0ac795a920612856a91fd81449
pradyyadav <pradyumna.aky@gmail.com>
2021-02-04 13:18:03 +0530

    dev_update

--- api/views.py
+++ api/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(response):
-    return HttpResponse("Yeah this guy is working good")
+    return HttpResponse("Yeah this guy is working good but in dev branch")
