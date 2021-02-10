31dd27b1ad3ab988b91e9e77716be0abff08ecfe
pradyyadav <pradyumna.aky@gmail.com>
2021-02-10 15:20:32 +0530

    hide : secret key

--- tf_learn/settings.py
+++ tf_learn/settings.py
@@ -1,15 +1,21 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#env_config
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+n1u+%lc34b)vu3(c_*63ty@2xnb0y1!+^b&=q^7o^bjs8zzm$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
